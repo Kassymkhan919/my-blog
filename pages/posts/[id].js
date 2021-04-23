b95540955806012fe1ec43d292cb9503295219fb
kassymkhan bekbolatov <kbekbolatov@biointeractivetech.com>
2021-04-22 23:50:37 -0700

    initial commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+
+export default function Post({ postData }) {
+  return (  
+    <Layout>
+      <Head>
+        {postData.title}
+      </Head>
+      <article >
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <br />
+      </article>
+      <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
+    </Layout>
+  )
+}
+
+// needed to get paths for pages
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+// needed to get content for pages
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
---
+++ pages/posts/whoiskaltoska.js
@@ -0,0 +1,3 @@
+export default function Whoiskaltoska() {
+    return <h1>It is zhanya</h1>
+}
