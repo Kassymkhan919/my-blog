d58eb5ad1dcd8193849004e65740e202e44ca445
Kassymkhan Bekbolatov <kasymhan007@gmail.com>
2025-02-20 15:26:27 -0800

    update main page

--- pages/index.js
+++ pages/index.js
@@ -8,24 +8,14 @@ const IndexPage = () => {
         <div className="max-w-xl mx-12 my-32">
           <h1 className="font-display text-5xl">Hello, I'm Kasym.</h1>
           <div className="mt-4 font-light text-xl  font-extra">
-            <p className="text-company">Front-end and iOS</p> Software Developer{''}
+            <p className="text-company">Data and Inrastructure </p> Software Engineer{''}
             {/* <a className="text-company hover:text-company-dark" href="#" target="_self" rel="noopener noreferrer">
               Google
             </a> */}
             , Vancouver
           </div>
 
-          <div className="mt-2 font-light text-sm  font-extra">
-            Previously at {''}
-            {/* <h3 className="text-gray-500 hover:text-blue-hover">BioInteractive Tech</h3> */}
-            <a className="text-gray-500 hover:text-blue-hover" href="https://www.tenzrhealth.com/" target="_blank" rel="noopener noreferrer">
-              BioInteractive Tech
-            </a>
-            ,{' '}
-            <a className="text-gray-500 hover:text-blue-hover" href="#" target="_self" rel="noopener noreferrer">
-              Amcama
-            </a>
-          </div>
+          
         </div>
       </div>
     </Layout>
